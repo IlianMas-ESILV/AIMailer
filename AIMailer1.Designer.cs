e448acb9a7bfe571ca9b96b282206b9e2c8856ff
IlianLance-ESILV <ilian.mas@edu.devinci.fr>
2025-06-16 19:34:04 +0200

    v0.20 Menu IA horizontal

--- AIMailer1.Designer.cs
+++ AIMailer1.Designer.cs
@@ -8,16 +8,16 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AiMailer.Properties {
+namespace AiMailer {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    internal sealed partial class AIMailer : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        private static AIMailer defaultInstance = ((AIMailer)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new AIMailer())));
         
-        public static Settings Default {
+        public static AIMailer Default {
             get {
                 return defaultInstance;
             }
